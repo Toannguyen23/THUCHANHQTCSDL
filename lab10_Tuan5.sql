f9ad8324b72b0cf3ec2ced2462c625434828e698
Toannguyen23 <118584306+Toannguyen23@users.noreply.github.com>
2023-04-19 10:25:17 +0700

    Add files via upload

---
+++ lab10_Tuan5.sql
@@ -0,0 +1,47 @@
+﻿CREATE LOGIN TruongLV WITH PASSWORD = '123456789';
+go
+
+USE QLBanHang;
+
+CREATE USER TruongLV FOR LOGIN TruongLV;
+
+go
+
+USE master;
+
+GRANT CREATE DATABASE TO TruongLV;
+go
+
+USE QLBanHang;
+
+GRANT CREATE TABLE TO TruongLV;
+go
+--tạo và phân quyền cho AnhLV
+CREATE LOGIN AnhLV WITH PASSWORD = '123456789';
+go
+
+USE QLBanHang;
+
+CREATE USER AnhLV FOR LOGIN AnhLV;
+
+go
+USE QLBanHang;
+
+GRANT VIEW ANY DATABASE TO AnhLV;
+go
+USE QLBanHang;
+
+GRANT INSERT ON dbo.PhongMay TO AnhLV;
+go
+-- sao lưu và khôi phục dữ liệu
+
+Backup database QLBanHang to disk = 'D:\NguyenMinhtoan_HeQTcosoDL\QLBanHang.bak'
+go
+--difference backup
+Backup database QLBanHang to disk = 'D:\NguyenMinhtoan_HeQTcosoDL\QLBanHang.bak' with differential
+
+
+-- bakup transaction 
+
+backup log QuanLyBanHang to disk = 'D:\NguyenMinhtoan_HeQTcosoDL\QLBH.trn'
+go
